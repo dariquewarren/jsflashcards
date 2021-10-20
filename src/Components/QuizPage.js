6cb405755f2a63472e3581b1f4e464f6f6323c28
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2021-10-20 00:55:06 -0400

    changed text on test button to log quiz data. added a button to shuffle flash cards. added shuffle functionality to the shuffle button. connected shuffled quiz data to flash cards

--- src/Components/QuizPage.js
+++ src/Components/QuizPage.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FlashCard from './FlashCard';
+import Button from 'react-bootstrap/Button'
 
 
 const QuizPage = (props)=> {
+const [quizData, changeQuizData] = useState(props.quiz)
+
+const shuffleQuiz = (theQuiz)=>{
+    var tempArray1 = []
+    var tempArray2 = []
+
+var i = 0
+for (i; i<theQuiz.length; i++){
+    if(theQuiz.indexOf(theQuiz[i]) % 2 ){
+        tempArray1.push(theQuiz[i])
+
+    }else{
+        tempArray2.push(theQuiz[i])
+    }
+}
+
+    var shuffledQuiz = tempArray1.concat(tempArray2)
+
+changeQuizData(shuffledQuiz)
+
+}
     return (
-        <div >
-<button onClick={()=> console.log(props.quiz)} > test button</button>
-{props.quiz.map((m)=>{
+        <div>
+        <h2>title Flash cards title</h2>
+<Button
+onClick={()=>shuffleQuiz(quizData)}
+>Shuffle cards</Button>
+<button onClick={()=> console.log('quizData',quizData)} > log quiz data</button>
+{quizData.map((m)=>{
   return (
       <div 
-      key={props.quiz.indexOf(m)+1}
+      key={quizData.indexOf(m)+1}
       style={{
           display: 'flex',
           flexDirection: 'row',
